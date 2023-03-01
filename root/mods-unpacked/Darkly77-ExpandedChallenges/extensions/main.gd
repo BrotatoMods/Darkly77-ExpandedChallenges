c222493aee21ec24fb7ccbc682663727ce791c6f
Chris Bloomfield <43499897+ithinkandicode@users.noreply.github.com>
2023-03-01 21:24:09 +0000

    Endless - Add comments to main.gd

--- root/mods-unpacked/Darkly77-ExpandedChallenges/extensions/main.gd
+++ root/mods-unpacked/Darkly77-ExpandedChallenges/extensions/main.gd
@@ -6,7 +6,9 @@ extends "res://main.gd"
 func clean_up_room(is_last_wave:bool = false, is_run_lost:bool = false, is_run_won:bool = false)->void :
 	.clean_up_room(is_last_wave, is_run_lost, is_run_won)
 
+	# Regular
 	if is_last_wave and not is_run_lost:
 		ChallengeService.extch_check_custom_completion_challenges(true)
+	# Endless
 	elif RunData.is_endless_run and RunData.current_wave >= 20 and is_last_wave and is_run_won:
 		ChallengeService.extch_check_custom_completion_challenges(true)
